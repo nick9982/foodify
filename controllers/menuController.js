a62733253a95917bd6410258a59eda2c36b33b95
praiseneh <somtoeneh@gmail.com>
2022-10-22 21:09:10 -0500

    added-menu-items

--- controllers/menuController.js
+++ controllers/menuController.js
@@ -8,7 +8,7 @@ const sqlConfig = {
     pool: {
         max: 10,
         min: 0,
-        idleTimeoutMillis: 30000,
+        idleTimeoutMillis: 90000,
     },
     options: {
         encrypt: true,
@@ -16,15 +16,25 @@ const sqlConfig = {
     },
 };
 
+const foodNames = ['Pasta', 'French Fries', 'Ice cream', 'Bread', 'Fried Rice', 'Pancakes', 'Burger', 'Pizza']
+
 const handleMenu = async (req, res) => {
-    try {
-        await sql.connect(sqlConfig);
-        var result = await sql.query('SELECT * FROM Employee');
-    } catch (error) {
-        throw error;
+    let totalRows = 0;
+    for (let index = 0; index < foodNames.length; index++) {
+        let price = Math.floor(Math.random() * 101);
+        let uniqueID = Math.floor(Math.random() * 101);
+        try {
+            await sql.connect(sqlConfig);
+            var result = await sql.query(`
+            INSERT INTO MenuItems VALUES (${uniqueID}, '${foodNames[index]}', ${price}, 'none', 'none')
+            `);
+        } catch (error) {
+            throw error;
+        }
+        // Result
+        totalRows = parseInt(totalRows) + parseInt(result.rowsAffected);
     }
-    // Result
-    res.json(result.recordset);
+    res.json({rowsAffected: totalRows});
 }
 
 module.exports = {handleMenu};
