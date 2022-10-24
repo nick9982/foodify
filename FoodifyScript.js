cc728d55a30d14ca04b2a7d94b17ce69ec0f67da
krypteccc <dredd1523@gmail.com>
2022-10-23 21:19:04 -0500

    Foodify

---
+++ FoodifyScript.js
@@ -0,0 +1,29 @@
+// this doesnt work
+$(document).ready(function(){
+    $("#div1").fadeIn();
+    $("#div1").animate({down: '50px'});
+});
+
+function validate() {
+    var username=document.getElementById("UN").value;
+    var password=document.getElementById("PW").value;
+    // Connect with backend here to validate user
+    
+    // Temp data
+    if(username=="admin" && password=="user") {
+        alert("Login successful.");
+        window.open("FoodifyMenu.html");
+    } else {
+        alert("Login failed.");
+    }
+}
+
+// Add item to menu with which item id is added
+function addItem() {
+
+}
+
+// Logout 
+function logout() {
+
+}
