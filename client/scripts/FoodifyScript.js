b97b20ce8f0183b4311ffbae2ec613879fc8f65d
nick9982 <nickmoenpm@gmail.com>
2022-10-25 20:06:46 -0500

    commit

--- client/scripts/FoodifyScript.js
+++ client/scripts/FoodifyScript.js
@@ -5,24 +5,29 @@
 
 //});
 
-const server = "http://localhost:8080"
+const server = "http://localhost:8080";
 
 function validate()
 {
     var username=document.getElementById("UN").value;
     var password=document.getElementById("PW").value;
-    let data = {
-        Username: username,
-        Password: password
-    };
 
-    login(data)
-        .then(response => response.json())
-        .then(data => {
-            alert(data["Error"]);
-            if(data["Error"] == "Authentication failed") window.location = "FoodifyLoginPage.html";
-            else window.location = "FoodifyMenu.html";
-        });
+    if(username == '' || password == '') alert("All fields required");
+    else
+    {
+        let data = {
+            Username: username,
+            Password: password
+        };
+
+        login(data)
+            .then(response => response.json())
+            .then(data => {
+                alert(data["Error"]);
+                if(data["Error"] == "Authentication failed") window.location = "FoodifyLoginPage.html";
+                else window.location = "FoodifyMenu.html";
+            });
+    }
 
 }
 
@@ -43,17 +48,69 @@ const login = async (data) =>
     });
 
     return response;
-}
+};
 
 function register() {
+    var name = document.getElementById("NM").value;
     var username=document.getElementById("UN").value;
     var password=document.getElementById("PW").value;
-    // Connect with backend to create new account
+    var re_enteredPassword = document.getElementById("RPW").value;
+    const buttons = document.getElementsByClassName("radiobtn");
+    var role = '';
+    for(const button of buttons)
+    {
+        if(button.checked) role = button.value;
+        break;
+    }
+    if(name == '' || username == '' || password == '' || re_enteredPassword == '' || role == '')
+    {
+        alert('All fields required');
+    }
+    else
+    {
+        // Connect with backend to create new account
+        let data = {
+            Username: username,
+            Password: password,
+            Re_enteredPassword: re_enteredPassword,
+            Name: name,
+            Employee_type: role
+        };
 
-    alert("new account created");
+        create_acc(data)
+            .then(response => response.json())
+            .then(data => {
+                alert(data["Error"]);
+                if(data["Error"] == "Account created successfully") window.location = 'FoodifyLoginPage.html';
+            });
+    }
+}
+
+const create_acc = async (data) =>
+{
+    const response = await fetch(server + "/create_account", {
+        method: 'POST',
+        mode: 'cors',
+        cache: 'no-cache',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        redirect: 'follow',
+        reffererPolicy: 'no-refferer',
+        body: JSON.stringify(data)
+    });
+
+    return response;
+};
+
+function toRegPage()
+{
+    window.location = "FoodifyRegisterPage.html";
 }
 
 // Logout 
 function logout() {
-
+    window.location = "FoodifyLoginPage.html";
 }
